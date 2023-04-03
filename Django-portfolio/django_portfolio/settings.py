8081650dc4ae8c55255d5a63b67aa2121d766101
Jose Pauluk <josepauluk@gmail.com>
2023-04-03 16:20:57 -0300

    debug and allowed host change

--- Django-portfolio/django_portfolio/settings.py
+++ Django-portfolio/django_portfolio/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [54.207.117.55]
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = [54.207.117.55]
+ALLOWED_HOSTS = []
 
 
 
